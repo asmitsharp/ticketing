7b47a1fb1b0f9d1a2669a104ef89300c37ad5ce2
Asmit Singh <ashmitsingh068@gmail.com>
2024-03-22 11:30:19 +0530

    async error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors'
 import { json } from 'body-parser';
 
 import { currentUserRouter } from './routes/current-user';
@@ -6,6 +7,8 @@ import { signoutRouter } from './routes/signout';
 import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import { textChangeRangeIsUnchanged } from 'typescript';
 
 const app = express();
 app.use(json());
@@ -14,6 +17,11 @@ app.use(currentUserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError;
+  
+})
 
 app.use(errorHandler);
 
