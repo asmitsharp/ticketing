f486fdf4f0b3d3ce40f829fdede6b142cc845986
Asmit Singh <ashmitsingh068@gmail.com>
2024-04-30 16:17:13 +0530

    current-user, signin and signout implementation and also implementing require-auth and current-user middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,11 +1,11 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from "jsonwebtoken";
 
-import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
 import { BadRequestError } from "../errors/bad-request-error";
 import { User } from "../models/users";
+import { validateRequest } from "../middlewares/validate-request";
 
 const router = express.Router();
 
@@ -14,12 +14,9 @@ router.post('/api/users/signup', [
   body('email').isEmail().withMessage('Email must be valid'),
   body('password').trim().isLength({min: 4, max: 20}).withMessage('Password must be between 4 and 20 characters')
 
-] , async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-
-    if(!errors.isEmpty()){
-      throw new RequestValidationError(errors.array())
-    }
+] ,
+validateRequest ,
+async (req: Request, res: Response) => {
 
     const { email, password }  = req.body;
 
