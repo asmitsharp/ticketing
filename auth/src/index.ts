ebf1aada35972acdd7c664e9822d4793e4723be4
Asmit Singh <ashmitsingh068@gmail.com>
2024-04-30 14:58:50 +0530

    authentication and verifaction using jwtand cookie

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import 'express-async-errors'
 import { json } from 'body-parser';
+import mongoosecd from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signoutRouter } from './routes/signout';
@@ -8,10 +10,17 @@ import { signinRouter } from './routes/signin';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
-import { textChangeRangeIsUnchanged } from 'typescript';
+import mongoose from 'mongoose';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signupRouter);
@@ -25,6 +34,21 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
+const start = async () => {
+  if(!process.env.JWT_KEY){
+    throw new Error('Key must be defined.')
+  }
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017');
+    console.log('Connected to mongodb');
+    
+  } catch (err) {
+    console.error(err);
+  }
+}
+
 app.listen(3000, () => {
   console.log('Listening on Port: 3000.!!!');
 });
+
+start();
