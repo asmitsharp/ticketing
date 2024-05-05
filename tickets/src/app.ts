0015d6ad84ba3aa371f74ae11ec43c1e277bcf4e
Asmit Singh <ashmitsingh068@gmail.com>
2024-05-05 20:15:53 +0530

    ticket-service development completed

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-import 'express-async-errors'
+import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
-import { errorHandler, NotFoundError } from '@ticketscx/common';
-
+import { errorHandler, NotFoundError, currentUser } from '@ticketscx/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,12 +17,16 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+app.use(currentUser);
 
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
-app.all('*', async () => {
-  throw new NotFoundError;
-  
-})
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
